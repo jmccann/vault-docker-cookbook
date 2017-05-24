d54c8a7deb2604d3f5808c6e57a90538393cb395
Jacob McCann <jacob.mccann2@target.com>
2017-05-24 13:05:20 -0500

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name 'vault-docker'
+maintainer 'Jacob McCann'
+maintainer_email 'jacob.mccann2@target.com'
+license 'Apache-2.0'
+description 'Installs/Configures vault-docker'
+long_description 'Installs/Configures vault-docker'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+source_url 'https://github.com/jmccann/vault-docker-cookbook'
+issues_url 'https://github.com/jmccann/vault-docker-cookbook/issues'
