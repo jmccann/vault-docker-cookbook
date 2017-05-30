7a98178b751a2c44a04e741cd9326cb3893e9cbe
Jacob McCann <jacob.mccann2@target.com>
2017-05-30 11:18:24 -0500

    Add test for IPC_LOCK

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -8,3 +8,7 @@
 describe port(8200) do
   it { should be_listening }
 end
+
+describe command ('docker logs vault') do
+  its(:stderr) { should_not include("Couldn't start vault with IPC_LOCK")}
+end
