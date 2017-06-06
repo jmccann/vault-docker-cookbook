c70675d5277b2047765a93f8902b64cb3911771e
Jacob McCann <jacob.mccann2@target.com>
2017-06-06 12:39:47 -0500

    Version 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'jacob.mccann2@target.com'
 license 'Apache-2.0'
 description 'Installs/Configures vault-docker'
 long_description 'Installs/Configures vault-docker'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 source_url 'https://github.com/jmccann/vault-docker-cookbook'
 issues_url 'https://github.com/jmccann/vault-docker-cookbook/issues'
 
-depends 'jmccann-docker-host', '~> 2.0'
+depends 'jmccann-docker-host', '~> 3.0'
 
 supports 'ubuntu', '>= 16.04'
